7e4fd958dbcc66bcc2836f5156e16c84c52b8bbe
Wacker <kab1634@ya.ru>
2024-05-22 20:08:26 -0300

    (feature): tree class methods added (not tested)

--- sources/storage.cpp
+++ sources/storage.cpp
@@ -1,21 +1,41 @@
 #include "storage.h"
 
+//Empty tree initialising
 Tree::Tree()
 {
-    Root = NULL;
+    this -> Root = NULL;
 }
 
-struct Elem Tree::Get(std::string KeyToFind)
+//Tree algorithm to get element in binary tree by some key
+Elem Tree::Get(std::string KeyToFind)
 {
-
+    TreeElem *tmpNode = Root;
+    //Binary tree search
+    while (tmpNode != NULL)
+    {
+        if (tmpNode -> Key == KeyToFind)
+        {
+            return *tmpNode;
+        }
+        else if (tmpNode -> Key < KeyToFind)
+        {
+            tmpNode = tmpNode -> Left;
+        }
+        else if (tmpNode -> Key > KeyToFind)
+        {
+            tmpNode = tmpNode -> Right;
+        }
+    }
+    return Elem();
 }
 
-struct Elem Tree::Put(struct Elem ElemToPut)
+//Tree algorithm to put element in binary tree
+Elem Tree::Put(struct Elem ElemToPut)
 {
     if (Root == NULL)
     {
-        Root = &TreeElem(ElemToPut);
-        return;
+        Root = new TreeElem(ElemToPut);
+        return Elem();
     }
     else
     {
@@ -26,8 +46,8 @@ struct Elem Tree::Put(struct Elem ElemToPut)
             {
                 if (tmpNode->Left == NULL)
                 {
-                    tmpNode->Left = &TreeElem(ElemToPut);
-                    return;
+                    tmpNode->Left = new TreeElem(ElemToPut);
+                    return Elem();
                 }
                 else
                 {
@@ -38,8 +58,8 @@ struct Elem Tree::Put(struct Elem ElemToPut)
             {
                 if (tmpNode->Right == NULL)
                 {
-                    tmpNode->Right = &TreeElem(ElemToPut);
-                    return;
+                    tmpNode->Right = new TreeElem(ElemToPut);
+                    return Elem();
                 }
                 else
                 {
@@ -48,25 +68,116 @@ struct Elem Tree::Put(struct Elem ElemToPut)
             }
             else if (tmpNode-> Key == ElemToPut.Key)
             {
-                struct Elem tmpElem;
-                tmpElem.Key = tmpNode -> Key;
-                tmpElem.Value = tmpNode -> Value;
-                tmpNode -> Value = ElemToPut.Value;
-                return tmpElem;
+                return Elem(*tmpNode);
             }
         }
     }
     std::cout << "Tree Put Error" << std::endl;
-}
-bool Tree::Del(std::string KeyToDelete)
-{
+    return Elem();
 }
 
-TreeElem::TreeElem(struct Elem ElemToInit)
+//Tree algorithm to delete any element by some key
+Elem Tree::Del(std::string KeyToDelete)
+{
+    TreeElem *tmpNodeCur = Root, *tmpNodePrev = NULL;
+    while (tmpNodeCur != NULL)
+    {
+        if (tmpNodeCur -> Key < KeyToDelete)
+        {
+            tmpNodePrev = tmpNodeCur;
+            tmpNodeCur = tmpNodeCur -> Left;
+        }
+        else if (tmpNodeCur -> Key > KeyToDelete)
+        {
+            tmpNodePrev = tmpNodeCur;
+            tmpNodeCur = tmpNodeCur -> Right;
+        }
+        else if (tmpNodeCur -> Key == KeyToDelete)
+        {
+            Elem RetElem(*tmpNodeCur);
+
+            tmpNodeCur -> DelElem(tmpNodePrev);
+            
+            return RetElem;
+        }
+    }
+    return Elem();
+}
+
+//Initializing TreeElem by inheritated Elem class
+TreeElem::TreeElem(Elem ElemToInit)
 {
     this->Key = ElemToInit.Key;
     this->Value = ElemToInit.Value;
 
     this->Right = NULL;
     this->Left = NULL;
+}
+
+void TreeElem::CopyData(TreeElem ElemToCopy)
+{
+    this -> Key = ElemToCopy.Key;
+    this -> Value = ElemToCopy.Value;
+}
+
+void TreeElem::DelElem(TreeElem *ParentElem)
+{
+    //If matched node has 0 childs
+    if (this -> Right == NULL && this -> Left == NULL)
+    {
+        if (ParentElem -> Left == this)
+            ParentElem -> Left = NULL;
+        else if (ParentElem -> Right == this)
+            ParentElem -> Right = NULL;
+        delete this;
+    }
+    //If matched node has 2 childs -- replace matched node with the minimum node in the right part of tree
+    else if (this -> Right != NULL && this -> Left != NULL)
+    {
+        TreeElem *tmpNodeRightMinPrev = this;
+        TreeElem *tmpNodeRightMin = this -> Right;
+        
+        //Finding minimum node in the right part of tree
+        while (tmpNodeRightMin -> Left != NULL)
+        {
+            tmpNodeRightMinPrev = tmpNodeRightMin;
+            tmpNodeRightMin = tmpNodeRightMin -> Left;
+        }
+
+        this->CopyData(*tmpNodeRightMin);
+
+        delete tmpNodeRightMin;
+        tmpNodeRightMinPrev -> Left = NULL;
+
+    }
+    //If matched node has only 1 child
+    else
+    {
+        TreeElem *tmpNodeChild = NULL;
+        if (this -> Right != NULL)
+            tmpNodeChild = this -> Right;
+        else if (this -> Left != NULL)
+            tmpNodeChild = this -> Left;
+
+        if (ParentElem -> Right == this)
+            ParentElem -> Right = tmpNodeChild;
+        else if (ParentElem -> Left == this)
+            ParentElem -> Left = tmpNodeChild;
+        
+        delete this;
+    }
+}
+
+//Initialising empty Elem object
+Elem::Elem()
+{
+    this -> Key = "";
+    this -> Value = "";
+}
+
+//Initialising Elem with ElemTree object
+Elem::Elem(TreeElem ElemToInit)
+{
+    this -> Key = ElemToInit.Key;
+    this -> Value = ElemToInit.Value;
 }
--- sources/storage.h
+++ sources/storage.h
@@ -1,52 +1,68 @@
 #include <iostream>
 
+class TreeElem;
+class Tree;
+class Elem;
+
+class StorageInterface
+{
+    virtual Elem Get(std::string);
+    virtual Elem Put(struct Elem);
+    virtual Elem Del(std::string);
+};
+
 class Storage
 {
     int CountElems;
-    StorageInterface& StorageInstance;
+    StorageInterface &StorageInstance;
 
 public:
     Storage(StorageInterface&);
     ~Storage();
 
-    struct Elem* Get(std::string );
+    Elem* Get(std::string );
     bool Put(std::string, std::string);
     bool Del(std::string);
-    struct Elem& operator[](std::string);
+    Elem& operator[](std::string);
 
 };
 
-class StorageInterface
+
+class Elem
 {
-    virtual struct Elem Get(std::string);
-    virtual struct Elem Put(struct Elem);
-    virtual bool Del(std::string);
+    friend class TreeElem;
+    friend class Tree;
+protected:
+    std::string Key;
+    std::string Value;
+public:
+    Elem();
+    Elem(std::string, std::string);
+    Elem(TreeElem);
+};
+
+class TreeElem : public Elem
+{
+    friend class Tree;
+protected:
+    void CopyData(TreeElem);
+    void DelElem(TreeElem*);
+public:
+    TreeElem* Left;
+    TreeElem* Right;
+
+    TreeElem(Elem);
 };
 
 class Tree : public StorageInterface
 {
     TreeElem* Root;
-    
+
 public:
     Tree();
     ~Tree();
 
-    struct Elem Get(std::string) override;
-    struct Elem Put(struct Elem) override;
-    bool Del(std::string) override;
-};
-
-struct Elem
-{
-    std::string Key;
-    std::string Value;
-};
-
-class TreeElem : public Elem
-{
-public:
-    struct TreeElem* Left;
-    struct TreeElem* Right;
-
-    TreeElem(struct Elem);
+    Elem Get(std::string) override;
+    Elem Put(struct Elem) override;
+    Elem Del(std::string) override;
 };
